93c27ef8b3b20d20164d2d8702e9ec6de7c87806
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2024-01-24 23:18:07 +0100

    Password Validator Kata - TDD

---
+++ src/passwordValidator.ts
@@ -0,0 +1,14 @@
+export const passwordValidator = (password: string) => {
+    const passwordContainsNumbers = (password:string) => /\d/.test(password)
+    const passwordContainsCapitalLetter = (password:string) =>/[A-Z]/.test(password)
+    const passwordContainsLowerCaseLetter = (password:string) => /[a-z]/.test(password)
+    const passwordContainsUnderscore = (password:string) => /_/.test(password)
+    if (password.length > 5
+        && passwordContainsNumbers(password)
+        && passwordContainsCapitalLetter(password)
+        && passwordContainsLowerCaseLetter(password)
+        && passwordContainsUnderscore(password)){
+        return true
+    }
+    return false
+}
