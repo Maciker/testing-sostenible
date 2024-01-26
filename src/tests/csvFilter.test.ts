ce02656158645f908020f26786a2f47987962e6c
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2024-01-26 08:52:21 +0100

    Business rules for csv filter kata

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -1,3 +1,11 @@
+/*
+Es válido que algunos campos estén vacíos, apareciendo dos comas seguidas o una coma final.
+El número de factura no puede estar repetido, si lo estuviese eliminaremos todas las líneas con repetición.
+Los impuestos IVA e IGIC son excluyentes, es decir, sólo puede aplicarse uno de los dos. Si alguna línea tiene contenido en ambos campos debe quedarse fuera.
+Los campos CIF y NIF son excluyentes, sólo se puede usar uno de ellos.
+El neto es el resultado de aplicar al bruto el correspondiente impuesto. Si algún neto no está bien calculado la línea se queda fuera.
+*/
+
 describe('CSV filter kata tests', ()=> {
 
 })
