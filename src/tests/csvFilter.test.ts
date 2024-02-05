a3b73c1a57deef6251ca3c3979c39f676c7ebbce
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2024-02-05 22:47:28 +0100

    Update csvFilter.test.ts

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -1,11 +1,23 @@
-/*
-Es válido que algunos campos estén vacíos, apareciendo dos comas seguidas o una coma final.
-El número de factura no puede estar repetido, si lo estuviese eliminaremos todas las líneas con repetición.
-Los impuestos IVA e IGIC son excluyentes, es decir, sólo puede aplicarse uno de los dos. Si alguna línea tiene contenido en ambos campos debe quedarse fuera.
-Los campos CIF y NIF son excluyentes, sólo se puede usar uno de ellos.
-El neto es el resultado de aplicar al bruto el correspondiente impuesto. Si algún neto no está bien calculado la línea se queda fuera.
-*/
+export class CsvFilter {
+    private constructor(private readonly lines: string[]) {}
 
-describe('CSV filter kata tests', ()=> {
+    static create(lines: string[]){
+        return new CsvFilter(lines)
+    }
 
-})
+    get filteredLines() {
+        const result = [];
+        result.push(this.lines[0]);
+        const fields = this.lines[1].split(',');
+        const ivaField = fields[4];
+        const igicField = fields[5];
+        const decimalRegex = '\\d+(\\.\\d+)?';
+        const taxFieldAreMutuallyExclusive =
+            (ivaField.match(decimalRegex) || igicField.match(decimalRegex)) && (!ivaField || !igicField);
+        if(taxFieldAreMutuallyExclusive) {
+            result.push(this.lines[1]);
+        }
+
+        return result;
+    }
+}
