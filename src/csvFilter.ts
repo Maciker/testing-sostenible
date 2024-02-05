6d3ce1ae7295c0aee58697ad4259020f1f78e6ac
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2024-02-05 22:48:08 +0100

    Update csvFilter.ts

--- src/csvFilter.ts
+++ src/csvFilter.ts
@@ -1,3 +1,23 @@
-export const csvFilter = (csv:string) => {
-    return csv
+export class CsvFilter {
+    private constructor(private readonly lines: string[]) {}
+
+    static create(lines: string[]){
+        return new CsvFilter(lines)
+    }
+
+    get filteredLines() {
+        const result = [];
+        result.push(this.lines[0]);
+        const fields = this.lines[1].split(',');
+        const ivaField = fields[4];
+        const igicField = fields[5];
+        const decimalRegex = '\\d+(\\.\\d+)?';
+        const taxFieldAreMutuallyExclusive =
+            (ivaField.match(decimalRegex) || igicField.match(decimalRegex)) && (!ivaField || !igicField);
+        if(taxFieldAreMutuallyExclusive) {
+            result.push(this.lines[1]);
+        }
+
+        return result;
+    }
 }
