41f4db6c12462cc00e466da7b0c3e697c814ec86
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2024-02-07 14:22:56 +0100

    Fibonacci logic

---
+++ src/fibonacci.ts
@@ -0,0 +1,9 @@
+export const fibonacci = (number: number) => {
+    if (number === 0) {
+        return 0
+    }
+    if (number === 1) {
+        return 1
+    }
+    return fibonacci(number - 1) + fibonacci(number - 2)
+}
