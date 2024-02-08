92c699ce709990716cb38166654f0adac289a288
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2024-02-08 23:13:13 +0100

    Update primeFactors.test.ts

--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -2,6 +2,6 @@ import {primeFactors} from "../primeFactors";
 
 describe('Prime Factors', () => {
   it('should return 2 if the given number it is 2', () => {
-      expect(primeFactors(2)).toBe(2)
-})  
+      expect(primeFactors(2)).toEqual([2])
+  })
 })
