5095ac1a8241dd104094415ec0f06efe75984613
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2024-02-08 23:37:04 +0100

    Word wrap tests

--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -3,5 +3,8 @@ import {wordWrap} from "../wordWrap";
 describe('WordWrap Tests', () => {
     it('Given a empty string should return an empty string', () => {
         expect(wordWrap('', 5)).toEqual('')
+    });
+    it('should return the word if the word length is less than the line length', () => {
+        expect(wordWrap('hello', 5)).toEqual('hello')
     })
 })
