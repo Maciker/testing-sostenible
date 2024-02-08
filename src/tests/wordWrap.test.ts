31029905c1e5fcb8066224bdf010051fe3192b0f
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2024-02-08 23:35:21 +0100

    First Word Wrap test

--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -1,13 +1,7 @@
-/*wordWrap('',5) ⇒ ''
-wordWrap('hello',5) ⇒ 'hello'
-wordWrap('longword',4) ⇒ 'long\nword'
-wordWrap('reallylongword',4) ⇒ 'real\nlylo\nngwo\nrd'
-wordWrap('abc def',4) ⇒ 'abc\ndef'
-wordWrap('abc def ghi',4) ⇒ 'abc\ndef\nghi'
-wordWrap(' abcdf',4) ⇒ '\nabcd\nf'
-wordWrap(null,5) ⇒ ''
-wordWrap('hello',-5) ⇒ throw exception*/
+import {wordWrap} from "../wordWrap";
 
 describe('WordWrap Tests', () => {
-    it('Given a empty string should return an empty string')
+    it('Given a empty string should return an empty string', () => {
+        expect(wordWrap('', 5)).toEqual('')
+    })
 })
