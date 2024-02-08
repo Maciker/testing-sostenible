9af73fc42e710add52a0f2b72eb329673c8640ff
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2024-02-08 23:27:19 +0100

    Course code for prime factors tests

--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -1,7 +1,20 @@
-import {primeFactors} from "../primeFactors";
+import {getPrimeFactorsFor} from "../primeFactors";
 
-describe('Prime Factors', () => {
-  it('should return 2 if the given number it is 2', () => {
-      expect(primeFactors(2)).toEqual([2])
-  })
-})
+describe('The prime factors', () => {
+    it('knows that the first prime is nomber one', () => {
+        expect(getPrimeFactorsFor(1)).toEqual([1]);
+    });
+    it('only accepts positive numbers', () => {
+        expect(() => getPrimeFactorsFor(-5)).toThrow();
+    });
+    it('knows what is a prime number', () => {
+        expect(getPrimeFactorsFor(2)).toEqual([2]);
+        expect(getPrimeFactorsFor(3)).toEqual([3]);
+    });
+    it('produces the same result to multiply the numbers in the output list', () => {
+        expect(getPrimeFactorsFor(2 * 2 * 2)).toEqual([2, 2, 2]);
+    });
+    it('orders the prime factors from the smallest to the biggest', () => {
+        expect(getPrimeFactorsFor(5 * 7 * 11 * 3)).toEqual([3, 5, 7, 11]);
+    })
+});
