8047d0d7a8ad1f52fd182f08ad02f31ca3d03a46
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2024-02-08 23:10:13 +0100

    Kata Prime Factors first test

--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -1,3 +1,7 @@
-describe('Prime Factors', () = > {
+import {primeFactors} from "../primeFactors";
 
+describe('Prime Factors', () => {
+  it('should return 2 if the given number it is 2', () => {
+      expect(primeFactors(2)).toBe(2)
+})  
 })
