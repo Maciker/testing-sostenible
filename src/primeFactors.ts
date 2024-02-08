27584a81dc939465787d9f4a73d64d4da8438526
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2024-02-08 23:26:53 +0100

    Course code for primeFactors

--- src/primeFactors.ts
+++ src/primeFactors.ts
@@ -1,3 +1,23 @@
-export const primeFactors = (value:number) => {
-    return [2]
+export function getPrimeFactorsFor(number: number) {
+    checkForPositiveNumber(number);
+    return primeFactors(number);
+}
+
+function checkForPositiveNumber(number: number) {
+    if (number < 1) throw new Error('Only positive numbers are allowed');
+}
+
+function primeFactors(number: number) {
+    const prime = findSmallestPrime(number);
+    const remainder = number / prime;
+    return remainder <= 1 ? [prime] : [prime].concat(getPrimeFactorsFor(remainder));
+}
+
+function findSmallestPrime(number: number) {
+    if (number === 1) return 1;
+    let factor = 2;
+    while (number % factor !== 0) {
+        ++factor;
+    }
+    return factor;
 }
