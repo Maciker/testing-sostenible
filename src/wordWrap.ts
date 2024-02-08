806d3dc7174f827d410b7bd385484971e3c45b1f
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2024-02-08 23:52:47 +0100

    Solve the problem with a recursive solution

--- src/wordWrap.ts
+++ src/wordWrap.ts
@@ -2,5 +2,7 @@ export const wordWrap = (words: string, lineLength: number) => {
     if (words.length <= lineLength) {
         return words
     }
-    return words.slice(0,4).concat('\n').concat(words.slice(4))
+    const wrappedText = words.slice(0,lineLength).concat('\n')
+    const unwrappedText = words.slice(lineLength);
+    return wrappedText.concat(wordWrap(unwrappedText, lineLength));
 }
